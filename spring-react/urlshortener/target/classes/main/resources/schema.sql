de07eaa5109ce1b01bb92d3f23b9a041148c50f3
fadihatem <fadster2003@yahoo.ca>
2023-10-19 11:13:16 -0400

    Delete spring-react/urlshortener/target directory

--- spring-react/urlshortener/target/classes/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `url` (
-                       `url_id` bigint NOT NULL AUTO_INCREMENT,
-                       `shortenedurl` varchar(255) DEFAULT NULL,
-                       `url` varchar(255) DEFAULT NULL,
-                       PRIMARY KEY (`url_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
